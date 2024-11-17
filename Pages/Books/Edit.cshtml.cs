4dbf408c89e477f8d2b332b8735c4d4e9f951830
nicolaadavid <davidnicola62@gmail.com>
2024-11-17 22:38:39 +0200

    Edit.cshtml.cs

---
+++ Pages/Books/Edit.cshtml.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using lab2_proiect_proba_refacut.Data;
+using lab2_proiect_proba_refacut.Models;
+
+namespace lab2_proiect_proba_refacut.Pages.Books
+{
+    public class EditModel : PageModel
+    {
+        private readonly lab2_proiect_proba_refacut.Data.lab2_proiect_proba_refacutContext _context;
+
+        public EditModel(lab2_proiect_proba_refacut.Data.lab2_proiect_proba_refacutContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book =  await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            Book = book;
+            ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID", "PublisherName");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Book).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookExists(Book.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool BookExists(int id)
+        {
+            return _context.Book.Any(e => e.ID == id);
+        }
+    }
+}
