c8945a4bb475e3a993a95c6fbf3ef4e2c29aaade
nicolaadavid <davidnicola62@gmail.com>
2024-11-17 22:43:21 +0200

    Delete.cshtml.cs

---
+++ Pages/Publishers/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using lab2_proiect_proba_refacut.Data;
+using lab2_proiect_proba_refacut.Models;
+
+namespace lab2_proiect_proba_refacut.Pages.Publishers
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly lab2_proiect_proba_refacut.Data.lab2_proiect_proba_refacutContext _context;
+
+        public DeleteModel(lab2_proiect_proba_refacut.Data.lab2_proiect_proba_refacutContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Publisher Publisher { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var publisher = await _context.Publisher.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Publisher = publisher;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var publisher = await _context.Publisher.FindAsync(id);
+            if (publisher != null)
+            {
+                Publisher = publisher;
+                _context.Publisher.Remove(Publisher);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
