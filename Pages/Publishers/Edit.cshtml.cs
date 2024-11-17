a50509dbad09d76ac0124420d483abaef07b2ea0
nicolaadavid <davidnicola62@gmail.com>
2024-11-17 22:45:30 +0200

    Create Edit.cshtml.cs

---
+++ Pages/Publishers/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using lab2_proiect_proba_refacut.Data;
+using lab2_proiect_proba_refacut.Models;
+
+namespace lab2_proiect_proba_refacut.Pages.Publishers
+{
+    public class EditModel : PageModel
+    {
+        private readonly lab2_proiect_proba_refacut.Data.lab2_proiect_proba_refacutContext _context;
+
+        public EditModel(lab2_proiect_proba_refacut.Data.lab2_proiect_proba_refacutContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Publisher Publisher { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var publisher =  await _context.Publisher.FirstOrDefaultAsync(m => m.ID == id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+            Publisher = publisher;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Publisher).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PublisherExists(Publisher.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool PublisherExists(int id)
+        {
+            return _context.Publisher.Any(e => e.ID == id);
+        }
+    }
+}
