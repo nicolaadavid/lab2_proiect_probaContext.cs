b8f30ed99a4fbe671af09c5446ce1272d27f21e9
nicolaadavid <davidnicola62@gmail.com>
2024-11-17 22:33:19 +0200

    Create Publisher.cs

---
+++ Models/Publisher.cs
@@ -0,0 +1,9 @@
+namespace lab2_proiect_proba_refacut.Models
+{
+    public class Publisher
+    {
+        public int ID { get; set; }
+        public string PublisherName { get; set; }
+        public ICollection<Book>? Books { get; set; }
+    }
+}
